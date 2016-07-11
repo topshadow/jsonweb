0b415e21eb3ed39ecbda6a50c6f92ba79fc222a6
topshadow <731502029@qq.com>
2016-07-11 14:11:45 +0800

    26行js代码实现json配置路由

--- www/all.js
+++ www/all.js
@@ -10,6 +10,7 @@ app.config(function($routeProvider){
 
     app.run( function($route, $http, $rootScope){
         $http.get("data/workspace/y001/customer.json").success(function(data){
+            $rootScope.data=data;
             var loop = 0, currentRoute;
             for(loop = 0; loop < data.pages.length; loop++){
                 currentRoute = data.pages[loop];
@@ -17,7 +18,8 @@ app.config(function($routeProvider){
                 console.log(routeName);
                 $routeProviderReference.when(routeName, {
                     url:routeName,
-                    templateUrl: currentRoute.templateUrl
+                    templateUrl: currentRoute.templateUrl,
+                    controller:'initDataControoler'
                 });
             }
             $route.reload();
@@ -25,6 +27,12 @@ app.config(function($routeProvider){
         });
     });
 
-
-
-
+app.controller('initDataControoler',function($rootScope,$scope,$location){
+// $scope.data=$rootScope.pages[]
+    var path = $location.path().replace('/','');
+   var  pages = $rootScope.data.pages;
+    for(var index in pages){
+        page.name==path;
+        $scope.data=pages[index];
+    }
+});
