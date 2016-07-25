a8ea3252ef2cdd119506ebe433b71440a2959e3f
topshadow <shadow2016>
2016-07-25 12:36:15 +0800

    动态路由实现成功

--- www/all.js
+++ www/all.js
@@ -1,38 +1,41 @@
-
-var app = angular.module('app', ['ngRoute']);
-
 var $routeProviderReference;
-var currentRoute;
-app.config(function($routeProvider){
-    $routeProviderReference = $routeProvider;
-});
 
-
-    app.run( function($route, $http, $rootScope){
+var app = angular.module('app',['ngRoute'] )
+    .config(function($routeProvider){
+        $routeProviderReference = $routeProvider;
+    }).run( function($route, $http, $rootScope){
+        http = $http;
         $http.get("data/workspace/y001/customer.json").success(function(data){
             $rootScope.data=data;
-            var loop = 0, currentRoute;
+            $rootScope.navbar =data.navbar;
+            $rootScope.footbar = data.footbar;
+            //
+            var loop , currentRoute;
             for(loop = 0; loop < data.pages.length; loop++){
                 currentRoute = data.pages[loop];
                 var routeName = "/" + currentRoute.name;
-                console.log(routeName);
+                console.log(routeName,currentRoute.templateUrl);
                 $routeProviderReference.when(routeName, {
                     url:routeName,
                     templateUrl: currentRoute.templateUrl,
-                    controller:'initDataControoler'
+                    controller:'initDataController'
                 });
             }
             $route.reload();
             $routeProviderReference.otherwise('/index');
         });
-    });
+    })
+        .controller('initDataController',function($rootScope,$scope,$location){
+            var path = $location.path().replace('/','');
+            var  pages = $rootScope.data.pages;
+            //search curennt Page Dats
 
-app.controller('initDataControoler',function($rootScope,$scope,$location){
-// $scope.data=$rootScope.pages[]
-    var path = $location.path().replace('/','');
-   var  pages = $rootScope.data.pages;
-    for(var index in pages){
-        page.name==path;
-        $scope.data=pages[index];
-    }
+            for(var i=0;i<pages.length;i++){
+                if(pages[i].name==path){
+                    $scope.data=pages[i];
+                }
+
+
+            }
+
 });
